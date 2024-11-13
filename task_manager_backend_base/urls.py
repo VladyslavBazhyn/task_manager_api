af4ef5c35a29578978701350a28e1ebb38842574
Gibbs <mr.darmstadtiumgmail.com>
2024-11-13 20:18:10 +0100

    Add url patterns of users and tasks apps to base url pattern

--- task_manager_backend_base/urls.py
+++ task_manager_backend_base/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("tasks/", include("task_manager_tasks.urls")),
+    path("users/", include("task_manager_users.urls"))
 ]
